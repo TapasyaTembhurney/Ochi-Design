2286a46dd6339e2afee82cb428d3d593f98c756c
TapasyaTembhurney <tapasyatembhurney@gmail.com>
2024-03-25 13:56:09 +0530

    Modified Eyes

--- src/Components/Eyes.jsx
+++ src/Components/Eyes.jsx
@@ -23,8 +23,8 @@ function Eyes() {
                             </div>
                         </div>
                     </div>
-                    <div className='flex items-center justify-center w-[16vw] h-[16vw] rounded-full bg-zinc-100'>
-                    <div className='relative w-2/3 h-2/3  rounded-full bg-zinc-900'>
+                    <div className='flex items-center justify-center w-[16vw] h-[16vw] rounded-full bg-zinc-100 '>
+                    <div className='relative w-2/3 h-2/3  rounded-full bg-zinc-900'>PLAY
                     <div style={{transform: `translate(-50%,-50%) rotate(${rotate}deg)`}} className='line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full h-10'>
                                 <div className='w-10 h-10 rounded-full bg-zinc-100'></div>
                             </div>
